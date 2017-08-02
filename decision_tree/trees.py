d4fca5eb672edd7e7e8f0334e412895c0a9e0c2a
yxx <yxx@githup>
2017-08-02 18:57:21 +0800

    full code

--- decision_tree/trees.py
+++ decision_tree/trees.py
@@ -29,3 +29,19 @@ def splitDataSet(dataSet,axis,value):
                
 def chooseBestFeatureToSplit(dataSet):
     numFeatures=len(dataSet[0])-1
+    baseEntropy=calcShannonEnt(dataSet)
+    bestInfoGain=0.0,beatFeature=-1
+    for i in range(numFeatures):
+        featList=[example[i] for example in dataSet]
+        uniqueVals=set(featList)
+        newEntropy=0.0
+        for value in uniqueVals:
+            subDataSet=splitDataSet(dataSet,i,value)
+            prob=len(subDataSet)/float(len(dataSet))
+            newEntropy+= prob +calcShannonEnt(subDataSet)
+        infoGain=baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain=infoGain
+            bestFeature=i
+     return beatFeature
+
